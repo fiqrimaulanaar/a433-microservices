103fcce6558bbb744980203d3bb1ffcff3891fb8
Fiqri Maulana Arokhman <fiqrimaulanaarokhman@gmail.com>
2023-11-28 10:21:36 +0700

    update token

--- build_push_image_karsajobs_ui.sh
+++ build_push_image_karsajobs_ui.sh
@@ -1,5 +1,5 @@
 docker build -t ghcr.io/fiqrimaulanaar/karsajobs-ui:latest . #melakukan build docker images dari Dockerfile yang lokasinya sama dengan lokasi script shell berada dengan nama karsajobs-ui dan tag latest
 
-echo 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 | docker login ghcr.io -u fiqrimaulanaar --password-stdin #login dengan menggunakan token akun github
+echo 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 | docker login ghcr.io -u fiqrimaulanaar --password-stdin #login dengan menggunakan token akun github
 
 docker push ghcr.io/fiqrimaulanaar/karsajobs-ui:latest #push atau mengirim images ke ghcr.io atau github packages
